bc116db2689838e9145c18b37acc8c2d48120c4a
dannguyenmessi1705 <messiprohy@gmail.com>
2024-02-26 14:27:24 +0700

    PTIT

--- PTIT/ELAB2303_Class_MyStuff/Main.java
+++ PTIT/ELAB2303_Class_MyStuff/Main.java
@@ -1,2 +1,33 @@
-package PTIT.ELAB2303_Class_MyStuff;public class Main ;{
+package PTIT.ELAB2303_Class_MyStuff;
+
+public class Main {
+    public static void main(String[] args) {
+        MyStuff m1 = new MyStuff("PC");
+        MyStuff m2 = new MyStuff("PC");
+
+        if (m2.equals(m1))
+            System.out.println("value compared: same");
+        else
+            System.out.println("value compared: different");
+        // b. Viết thêm câu lệnh so sánh hai tham chiếu và in ra same/different nếu tham chiếu là giống/khác
+        if (m2.hashCode() == m1.hashCode())
+            System.out.println("reference compared: same");
+        else
+            System.out.println("reference compared: different");
+    }
+}
+class MyStuff {
+    private String name;
+
+    MyStuff(String name) {
+        this.name = name;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        MyStuff myStuff = (MyStuff) obj;
+        if (myStuff.name.equals(this.name)) {
+            return true;
+        }
+        return false;
+    }
 }
--- PTIT/OOP009_Slope_Point/Main.java
+++ PTIT/OOP009_Slope_Point/Main.java
@@ -1,2 +1,32 @@
-package PTIT.OOP009_Slope_Point;public class Main {
+package PTIT.OOP009_Slope_Point;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int test = Integer.parseInt(sc.nextLine());
+        while (test-- > 0) {
+            Point p1 = new Point(sc.nextInt(), sc.nextInt());
+            Point p2 = new Point(sc.nextInt(), sc.nextInt());
+            System.out.println(p1.slope(p2));
+        }
+    }
+}
+
+class Point {
+    private int x, y;
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public double slope(Point other) {
+        if (other.x == x) {
+            return -1;
+        }
+        if (other.y - y == 0 && other.x - x < 0) {
+           return  (double) (other.y - y) / (other.x - x) * (-1);
+        }
+        return (double) (other.y - y) / (other.x - x);
+    }
 }
