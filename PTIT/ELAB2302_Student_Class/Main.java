d256ff9510eee79b43eb1400137a39d56202dbfe
dannguyenmessi1705 <messiprohy@gmail.com>
2024-03-11 16:14:48 +0700

    PTIT

--- PTIT/ELAB2302_Student_Class/Main.java
+++ PTIT/ELAB2302_Student_Class/Main.java
@@ -1,2 +1,44 @@
-package PTIT.ELAB2302_Student_Class;public class Main {
+package PTIT.ELAB2302_Student_Class;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<Student> students = new ArrayList<>();
+        while(true) {
+            try {
+                students.add(new Student(sc.nextLine(), Integer.parseInt(sc.nextLine())));
+            } catch (Exception e){
+                break;
+            }
+        }
+        for (int i = 0; i < students.size(); i++) {
+            students.get(i).display();
+            if (i == students.size() - 1){
+                System.out.println(Student.numberOfStudent);
+            }
+        }
+
+    }
+}
+
+class Student {
+    private String name;
+    private int age;
+    public static int numberOfStudent = 0;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+        numberOfStudent++;
+    }
+    public void display(){
+        if (age >= 18) {
+            System.out.println(name);
+        }
+    }
+
 }
--- PTIT/INHERITANCE001_Student_class/Main.java
+++ PTIT/INHERITANCE001_Student_class/Main.java
@@ -1,2 +1,37 @@
-package PTIT.INHERITANCE001_Student_class;public class Main {
+package PTIT.INHERITANCE001_Student_class;
+
+public class Main {
+    public static void main(String[] $) {
+        Student s = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
+        System.out.println(s);
+        System.out.println("Study credits "+s.getCredit());
+        s.study();
+        System.out.println("Study credits "+s.getCredit());
+    }
+}
+
+class Person {
+    private String name;
+    private String address;
+    public Person(String name, String address){
+        this.name = name;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return name + " - " + address;
+    }
+}
+class Student extends Person {
+    private static int credit = 0;
+    public Student(String name, String address) {
+        super(name, address);
+    }
+    public void study() {
+        credit++;
+    }
+    public int getCredit() {
+        return credit;
+    }
 }
