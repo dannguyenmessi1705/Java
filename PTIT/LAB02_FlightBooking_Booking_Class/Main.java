280f485e88a8993415c521682ce4e7a734635d91
dannguyenmessi1705 <messiprohy@gmail.com>
2024-03-15 01:57:40 +0700

    PTIT

--- PTIT/LAB02_FlightBooking_Booking_Class/Main.java
+++ PTIT/LAB02_FlightBooking_Booking_Class/Main.java
@@ -1,2 +1,57 @@
-package PTIT.LAB02_FlightBooking_Booking_Class;public class Main {
+package PTIT.LAB02_FlightBooking_Booking_Class;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int test = Integer.parseInt(sc.nextLine());
+        while (test-- > 0){
+            String result = "";
+            FlightBooking fb = new FlightBooking();
+            String s = sc.nextLine();
+            String[] users = s.split("\\D+");
+            // regrex d+ and s+ to split string by number and space
+            String[] commands = s.split("\\d+");
+            for (int i = 0; i < users.length; i++) {
+                if (commands[i + 1].trim().equals("Booking")) {
+                    fb.bookSeat();
+                    result += String.format("[Seat Available: %d]", fb.getSeatAvailable());
+                }else if (commands[i+1].trim().equals("Confirmed")){
+                    result += String.format("--User %s Confirmed--[Seat Available: %d]", users[i], fb.getSeatAvailable());
+                }
+                else if (commands[i+1].trim().equals("Cancel")){
+                    fb.cancelSeat();
+                    result += String.format("[Seat Available: %d]", fb.getSeatAvailable());
+                }
+            }
+            System.out.println(result);
+        }
+    }
+
+}
+
+class FlightBooking {
+    public String flightId="OOP24";
+    private static int seatAvailable=180;
+
+    public FlightBooking(){
+        seatAvailable = 180;
+    }
+
+    public void bookSeat(){
+        if (seatAvailable>0)
+            seatAvailable--;
+    }
+    public void cancelSeat(){
+        if (seatAvailable<180)
+            seatAvailable++;
+    }
+    public void confirmBooking(){
+        return;
+    }
+    public int getSeatAvailable(){
+        return seatAvailable;
+    }
+
 }
--- PTIT/STACK001_Compress_Duplicate/Main.java
+++ PTIT/STACK001_Compress_Duplicate/Main.java
@@ -1,2 +1,44 @@
-package PTIT.STACK001_Compress_Duplicate;public class Main {
+package PTIT.STACK001_Compress_Duplicate;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public Queue<Integer> compressDuplicates(Stack<Integer> s){
+        Queue<Integer> result = new LinkedList<>();
+        while (!s.isEmpty()){
+            int count = 1;
+            int top = s.pop();
+            while (!s.isEmpty() && s.peek() == top){
+                s.pop();
+                count++;
+            }
+            result.add(count);
+            result.add(top);
+        }
+        return result;
+    }
+    public static void main (String[] $){
+        Scanner sc = new Scanner(System.in);
+        int test = Integer.parseInt(sc.nextLine());
+        while (test-- > 0){
+            Stack<Integer> s = new Stack<>();
+            String[] arr = sc.nextLine().split(" ");
+            for (int i = arr.length - 1; i >= 0; i--){
+                s.push(Integer.parseInt(arr[i]));
+            }
+            Main main = new Main();
+            Queue<Integer> result = main.compressDuplicates(s);
+            while (!result.isEmpty()){
+                if (result.size() == 1){
+                    System.out.print(result.poll());
+                    break;
+                }
+                System.out.print(result.poll() + " ");
+            }
+            System.out.println();
+        }
+    }
 }
