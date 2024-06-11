ab45382faad7c3fed7d2c3c02bec4cd31d806ff1
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-06-11 19:08:01 +0700

    chore: Refactor Test.java for improved readability and input handling

--- Test.java
+++ Test.java
@@ -1,5 +1,15 @@
-public class Test {
-	public static void main(String[] args){
-		System.out.println("Hello World");
+import java.util.*;
+import java.lang.*;
+
+class Main
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int test = Integer.parseInt(sc.nextLine());
+		while(test-- > 0) {
+			String op[] = sc.nextLine().split("\\s+");
+			System.out.println(Math.pow(Double.parseDouble(op[0]), Double.parseDouble(op[1])));
+		}
 	}
 }
