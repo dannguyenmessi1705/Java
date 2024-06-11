bc41b8c50a2833ce89cd653ca135c7377c401622
dannguyenmessi1705 <didannguyen17@gmail.com>
2024-06-11 19:08:21 +0700

    Refactor Main.java for improved code structure and readability

--- Test.java
+++ Test.java
@@ -1,15 +1,23 @@
-import java.util.*;
-import java.lang.*;
+import java.util.Scanner;
 
-class Main
+class Test
 {
+	private static int reverse(int n) {
+		int rev = 0;
+		while(n > 0) {
+			rev = rev * 10 + n % 10;
+			n /= 10;
+		}
+		return rev;
+	}
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc = new Scanner(System.in);
 		int test = Integer.parseInt(sc.nextLine());
+		int i = 1;
 		while(test-- > 0) {
-			String op[] = sc.nextLine().split("\\s+");
-			System.out.println(Math.pow(Double.parseDouble(op[0]), Double.parseDouble(op[1])));
+			String []op = sc.nextLine().split("\\s+");
+			System.out.println(reverse(reverse(Integer.parseInt(op[0])) + reverse(Integer.parseInt(op[1]))));
 		}
 	}
 }
